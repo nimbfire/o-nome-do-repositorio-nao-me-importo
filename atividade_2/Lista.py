84bafa5a8cff95e9a00a178f988d40ff32ec1a0e
Marcelo Soares <nimbfire@gmail.com>
2020-08-26 20:44:21 -0300

    Updated the lista object to have the insert and print options updated

--- atividade_2/Lista.py
+++ atividade_2/Lista.py
@@ -14,7 +14,7 @@ class Lista:
       retorno = "\033[96m---------------------------------------------------------------------------------------------------------|\033[00m\n"
       retorno += "\033[96m{: ^34} \033[00m|\033[96m {: ^30s}\033[00m | \033[96m{: ^34s} |\033[00m\n".format('endereço do nó', 'dado','proximo')
       no_auxiliar = self.inicio
-      
+
       while( no_auxiliar.proximo ):
         retorno += "{}   |\033[96m{:<30}\033[00m  | {}\n".format(no_auxiliar, no_auxiliar.dado , no_auxiliar.proximo)
         no_auxiliar = no_auxiliar.proximo
@@ -39,3 +39,27 @@ class Lista:
       self.inicio = No(valor)
 
     self.tamanho += 1
+
+  def inserir(self, posicao, valor):
+    '''
+    Adicionar um valor em uma posição específica na lista.
+    '''
+    if posicao = 0:
+      no = No(valor)
+      no.proximo = self.inicio
+      self.inicio = no
+    else:
+      anterior = self.inicio
+      for i in range( posicao - 1):
+        if anterior:
+          anterior = anterior.proximo
+      no = No(valor)
+      no.proximo = anterior.proximo
+      anterior.proximo = no
+    self.tamanho += 1
+
+  def imprimir(self):
+    aux = self.inicio
+    while (aux):
+      print(aux.dado, '\n')
+      aux = aux.proximo
