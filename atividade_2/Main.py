3302f4b9c9d769915e59a8714dd42f7640793dbc
Marcelo Soares <nimbfire@gmail.com>
2020-09-23 19:39:25 -0300

    Not sure what Im commiting here to e honest...

--- atividade_2/Main.py
+++ atividade_2/Main.py
@@ -9,3 +9,26 @@ lista.adicionar(30)
 lista.adicionar(1330)
 lista.adicionar('a')
 print(lista)
+
+
+lista2 = Lista()
+lista2.inserirOrdenado(1)
+lista2.inserirOrdenado(2)
+lista2.inserirOrdenado(3)
+lista2.inserirOrdenado(4)
+lista2.inserirOrdenado(10)
+lista2.inserirOrdenado(5)
+lista2.inserirOrdenado(4)
+
+lista2.inserirOrdenado(42)
+
+lista2.inserirOrdenado(41)
+lista2.inserirOrdenado(44)
+
+lista2.inserirOrdenado(40)
+
+lista2.inserirOrdenado(49)
+
+
+
+print(lista2)
